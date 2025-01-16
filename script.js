bd87f12a2b432198043d1b70a64ecd7682affee0
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-01-16 11:32:11 -0400

    Adicionando mensagem de erro

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function adicionarTarefa() {
 
     // Mensagem após o input ser realizado
-    let mensagem = "Tarefa adicionada com sucesso!";
+    let mensagemSucesso = "Tarefa adicionada com sucesso!";
 
     // Pega o valor do input e salva em outra variável
     let inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem;
+    document.getElementById("mensagem").textContent = mensagemSucesso;
 
     // Adiciona a tarefa digitada no input em uma lista ul
     let listaTarefas = document.getElementById("listaTarefas")
@@ -20,4 +20,10 @@ function adicionarTarefa() {
 
     // Esvazia o input após o usuário enviar a tarefa
     inputTarefa.value = ""
+
+    // Verifica se o input é vazio e impede o usuári de enviar esse input
+    if (tarefa == "") {
+        let mensagemErro = "Adicione uma tarefa antes de tentar enviar";
+        document.getElementById("mensagem").textContent = mensagemErro;
+    }
 }
