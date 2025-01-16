9e1d4f36106ffa7a082b650222cec83a1c311b0c
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-01-16 11:50:47 -0400

    Finalizando lógica do if

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function adicionarTarefa() {
 
     // Pega o valor do input e salva em outra variável
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+
+    const mensagem = document.getElementById("mensagem")
 
     // Verifica se o input é vazio e impede o usuário de envia-lo caso seja esse o caso
     if (tarefa == "") {
         // Mensagem de erro
         let mensagemErro = "Digite uma tarefa para adicionar a sua lista";
-        document.getElementById("mensagem").textContent = mensagemErro;
+        mensagem.textContent = mensagemErro;
     } else {
         // Mensagem de sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!";
-        document.getElementById("mensagem").textContent = mensagemSucesso;
+        mensagem.textContent = mensagemSucesso;
 
         // Adiciona a tarefa digitada no input em uma lista ul
-        let listaTarefas = document.getElementById("listaTarefas")
+        const listaTarefas = document.getElementById("listaTarefas")
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefa
         listaTarefas.appendChild(novaTarefa)
