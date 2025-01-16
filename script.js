63ac32907d56a2adcd37ebc57f53f81bac851a46
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-01-16 11:37:53 -0400

    Alterando a lógica do if

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function adicionarTarefa() {
 
-    // Mensagem após o input ser realizado
-    let mensagemSucesso = "Tarefa adicionada com sucesso!";
-
     // Pega o valor do input e salva em outra variável
     let inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagemSucesso;
+
+    // Verifica se o input é vazio e impede o usuári de enviar esse input
+    if (tarefa == "") {
+        let mensagemErro = "Digite uma tarefa para adicionar a sua lista";
+        document.getElementById("mensagem").textContent = mensagemErro;
+    } else {
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        document.getElementById("mensagem").textContent = mensagemSucesso;
+    }
 
     // Adiciona a tarefa digitada no input em uma lista ul
     let listaTarefas = document.getElementById("listaTarefas")
     let novaTarefa = document.createElement("li")
-
+ 
     // Pega o conteudo da variável tarefa
     novaTarefa.textContent = tarefa
 
@@ -20,10 +25,4 @@ function adicionarTarefa() {
 
     // Esvazia o input após o usuário enviar a tarefa
     inputTarefa.value = ""
-
-    // Verifica se o input é vazio e impede o usuári de enviar esse input
-    if (tarefa == "") {
-        let mensagemErro = "Adicione uma tarefa antes de tentar enviar";
-        document.getElementById("mensagem").textContent = mensagemErro;
-    }
 }
